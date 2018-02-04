5284ae49bc0231cafa2c73f0c05ef0ea6d33cf71
Eder <faneder829@gmail.com>
2018-02-04 22:26:17 +0800

    add pagination to threads

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,38 +4,9 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel panel-default">
-                    <div class="panel-heading">Forum Threads</div>
+                @include ('threads._list')
 
-                    <div class="panel-body">
-                        @foreach ($threads as $thread)
-                            <article>
-                                <div class="level">
-                                    <h4 class="flex">
-                                        <a href="{{ $thread->path() }}">
-                                            @if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
-                                                <strong>
-                                                    {{ $thread->title }}
-                                                </strong>
-                                            @else
-                                                {{ $thread->title }}
-                                            @endif
-                                        </a>
-                                    </h4>
-
-                                    <a href="{{ $thread->path() }}">
-                                        {{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count) }}
-                                    </a>
-                                </div>
-
-
-                                <div class="body">{{ $thread->body }}</div>
-                            </article>
-
-                            <hr>
-                        @endforeach
-                    </div>
-                </div>
+                {{ $threads->render() }}
             </div>
         </div>
     </div>
--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -79,7 +79,7 @@ class ReadThreadsTest extends TestCase
 
         $response = $this->getJson('threads?popular=1')->json();
 
-        $this->assertEquals([3, 2, 0], array_column($response, 'replies_count'));
+        $this->assertEquals([3, 2, 0], array_column($response['data'], 'replies_count'));
     }
 
     function test_a_user_can_request_all_replies_for_a_given_thread()
@@ -94,13 +94,13 @@ class ReadThreadsTest extends TestCase
         $this->assertEquals(2, $response['total']);
     }
 
-    function a_user_can_filter_threads_by_thouse_that_are_unawswered()
+    function a_user_can_filter_threads_by_thouse_that_are_unanwswered()
     {
         $thread = create('App\Thread');
         create('App\Reply', ['thread_id' => $thread->id]);
 
         $response = $this->getJson('threads?unanswered=1')->json();
 
-        $this->assertCount(1, $response); 
+        $this->assertCount(1, $response['data']);
     }
 }

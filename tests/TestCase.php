6353e5c8bbc547066d2ff73b61b03816bebadab0
Eder <faneder829@gmail.com>
2018-02-24 23:57:13 +0800

    clean code

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,7 +5,6 @@ namespace Tests;
 use App\Exceptions\Handler;
 use Illuminate\Contracts\Debug\ExceptionHandler;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
@@ -14,8 +13,6 @@ abstract class TestCase extends BaseTestCase
     protected function setUp()
     {
         parent::setUp();
-
-        // DB::statement('PRAGMA foreign_keys=on;');
 
         $this->disableExceptionHandling();
     }
